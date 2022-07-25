97e1b3b8ffad637b2fe9d69835f2fb8cc554532c
Rosalia Padoin Oliveira <koxelinho@icloud.com>
2022-07-25 14:41:52 -0300

    feat: desafio10

---
+++ challenges/desafio10.js
@@ -0,0 +1,12 @@
+db.produtos.find({
+  valoresNutricionais: {
+    $elemMatch: {
+      tipo: "proteínas",
+      percentual: {
+        $gte: 30, $lte: 50,
+      },
+    },
+  },
+}, {
+  nome: true, _id: false,
+});
