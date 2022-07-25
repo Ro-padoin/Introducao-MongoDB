81e7c865181052f0c5954289bf0962f06581f471
Rosalia Padoin Oliveira <koxelinho@icloud.com>
2022-07-25 17:11:51 -0300

    feat: desafio8

--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -1,22 +1,24 @@
-// db.produtos.updateMany({}, {
-//   $set: { vendasPorDia: [0, 0, 0, 0, 0, 0, 0] },
-// });
+db.produtos.updateMany({}, {
+  $addToSet: { vendasPorDia: { $each: [0, 0, 0, 0, 0, 0, 0] } },
+});
 
-// db.produtos.updateOne({
-//   nome: { $eq: "Big Mac" },
-// }, {
-//   $inc: {
-//     "vendasporDia.3": 60,
-//   },
-// });
+db.produtos.updateOne({
+  nome: { $eq: "Big Mac" },
+}, {
+  $inc: {
+    "vendasporDia.3": 60,
+  },
+});
 
-// db.produtos.updateMany({}, 
-//   {
-//     $inc: {
-//       "vendasporDia.6": 60,
-//     },
-//   });
+db.produtos.updateMany({
 
-// db.produtos.find({ ultimaModificacao: { $exists: true } }, {
-//   nome: true, vendasPorDia: true, _id: false,
-// });
+}, 
+  {
+    $inc: {
+      "vendasporDia.6": 120,
+    },
+  });
+
+db.produtos.find({ ultimaModificacao: { $exists: true } }, {
+  nome: true, vendasPorDia: true, _id: false,
+});
