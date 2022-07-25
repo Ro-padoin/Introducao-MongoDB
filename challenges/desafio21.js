0f6730a77477c31ac7d7816a4408d31c1da06fc6
Rosalia Padoin Oliveira <koxelinho@icloud.com>
2022-07-25 14:58:51 -0300

    feat: desafio21

---
+++ challenges/desafio21.js
@@ -0,0 +1,11 @@
+db.produtos.updateOne({
+  nome: { $eq: "Cheddar McMelt" },
+}, {
+    $pop: {
+      ingredientes: 1,
+    },
+  });
+
+  db.produtos.find({}, {
+    nome: true, ingredientes: true, _id: false,
+  });
